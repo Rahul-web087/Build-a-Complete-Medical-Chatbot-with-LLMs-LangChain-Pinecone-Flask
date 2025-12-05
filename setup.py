492673de901d62b19fd33efc6e34c75ab2fe4aed
Rahul-web087 <nayakrahul9028@gmail.com>
2025-12-05 14:33:14 +0530

    requirements add

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import  find_packages,setup
+
+setup(
+    name= "medical_chatbot",
+    version = "0.0.1",
+    author = "Rahul Nayak",
+    author_email = "nayakrahul9028@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+
+)
